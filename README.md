4c4dfef05fcf4561f5088fb7d777000f84ab4b68
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2026-07-11 22:18:30 +0700

    Update

--- README.md
+++ README.md
@@ -37,3 +37,4 @@
 | DB       | [HTML](/src/T03_pf_db/T03.html)       | [PDF](/src/T03_pf_db/T03.pdf)       |
 | Backend  | [HTML](/src/T04_pf_backend/T04.html)  | [PDF](/src/T04_pf_backend/T04.pdf)  |
 | Frontend | [HTML](/src/T05_pf_frontend/T05.html) | [PDF](/src/T05_pf_frontend/T05.pdf) |
+| Testing  | [HTML](/src/T06_pf_testing/T06.html)  | [PDF](/src/T06_pf_testing/T06.pdf)  |
--- build.ps1
+++ build.ps1
@@ -26,9 +26,9 @@
 # npm run html $folder
 # npm run pdf $folder
 
-$folder = "src\T02_js_ts"
-npm run html $folder
-npm run pdf $folder
+# $folder = "src\T02_js_ts"
+# npm run html $folder
+# npm run pdf $folder
 
 # $folder = "src\T02A_ts_esm"
 # npm run html $folder
@@ -46,9 +46,9 @@ npm run pdf $folder
 # npm run html $folder
 # npm run pdf $folder
 
-# $folder = "src\T06_pf_testing"
-# npm run html $folder
-# npm run pdf $folder
+$folder = "src\T06_pf_testing"
+npm run html $folder
+npm run pdf $folder
 
 # $folder = "src\T07_pf_deploy"
 # npm run html $folder
